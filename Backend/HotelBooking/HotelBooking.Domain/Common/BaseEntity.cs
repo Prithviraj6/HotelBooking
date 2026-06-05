ceffd51ea9292c1b500b2588552215bd0187f967
Prithviraj6 <prithvirajpatil666@gmail.com>
2026-06-05 23:10:36 +0530

    Update base entity

--- Backend/HotelBooking/HotelBooking.Domain/Common/BaseEntity.cs
+++ Backend/HotelBooking/HotelBooking.Domain/Common/BaseEntity.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace HotelBooking.Domain.Common
 {
-    internal class BaseEntity
+    public abstract class BaseEntity
     {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; } = false;
     }
 }
