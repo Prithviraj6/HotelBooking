d7000cd7277afc8e20778d769d5a9194e8283d89
Prithviraj6 <prithvirajpatil666@gmail.com>
2026-06-06 13:29:41 +0530

    Update: Added UnitOfWork

--- Backend/HotelBooking/HotelBooking.Infrastructure/UnitOfWork.cs
+++ Backend/HotelBooking/HotelBooking.Infrastructure/UnitOfWork.cs
@@ -1,10 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿using HotelBooking.Domain.Interfaces;
+using HotelBooking.Domain.Interfaces.Repositories;
+using HotelBooking.Infrastructure.Data;
 namespace HotelBooking.Infrastructure
 {
-    internal class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
+        private readonly AppDbContext _context;
+
+        public IUserRepository Users { get; private set; }
+        public IHotelRepository Hotels { get; private set; }
+        public IRoomRepository Rooms { get; private set; }
+        public IBookingRepository Bookings { get; private set; }
+        public IPaymentRepository Payments { get; private set; }
+        public IReviewRepository Reviews { get; private set; }
+        public IPromotionRepository Promotions { get; private set; }
+
+        public UnitOfWork(AppDbContext context,
+            IUserRepository users,
+            IHotelRepository hotels,
+            IRoomRepository rooms,
+            IBookingRepository bookings,
+            IPaymentRepository payments,
+            IReviewRepository reviews,
+            IPromotionRepository promotions)
+        {
+            _context = context;
+            Users = users;
+            Hotels = hotels;
+            Rooms = rooms;
+            Bookings = bookings;
+            Payments = payments;
+            Reviews = reviews;
+            Promotions = promotions;
+        }
+
+        public async Task<int> SaveChangesAsync()
+            => await _context.SaveChangesAsync();
+
+        public void Dispose()
+            => _context.Dispose();
     }
 }
