dc44b3fdc1c5078108614d3fd229e8e52e3038b2
Prithviraj6 <prithvirajpatil666@gmail.com>
2026-06-06 17:37:00 +0530

    Update: Added a Repository

---
+++ Backend/HotelBooking/HotelBooking.Infrastructure/Repositories/RoomTypeRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HotelBooking.Domain.Entities;
+using HotelBooking.Infrastructure.Data;
+
+namespace HotelBooking.Infrastructure.Repositories
+{
+    public class RoomTypeRepository : GenericRepository<RoomType>, IRoomTypeRepository
+    {
+        public RoomTypeRepository(AppDbContext context) : base(context) { }
+
+        public async Task<IEnumerable<RoomType>> GetByHotelAsync(int hotelId)
+            => await _dbSet
+                .Where(rt => rt.HotelId == hotelId && !rt.IsDeleted)
+                .ToListAsync();
+    }
+}
