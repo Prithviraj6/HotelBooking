838130b5058ac533a07ed82863fbd312393d741d
Prithviraj6 <prithvirajpatil666@gmail.com>
2026-06-06 17:33:29 +0530

    Update: Added RoomType Interface

---
+++ Backend/HotelBooking/HotelBooking.Domain/Interfaces/Repositories/IRoomTypeRepository.cs
@@ -0,0 +1,7 @@
+﻿using HotelBooking.Domain.Entities;
+using HotelBooking.Domain.Interfaces.Repositories;
+
+public interface IRoomTypeRepository : IGenericRepository<RoomType>
+{
+    Task<IEnumerable<RoomType>> GetByHotelAsync(int hotelId);
+}
